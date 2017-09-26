a1a03f6c9e9fc721504f18ab88df12dd9d7a20ab
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-09-26 16:45:54 +0200

    misc

--- src/main/java/ch/digitalfondue/synckv/MessageReceiver.java
+++ src/main/java/ch/digitalfondue/synckv/MessageReceiver.java
@@ -25,7 +25,7 @@ class MessageReceiver extends ReceiverAdapter {
         //
         Object payload = msg.getObject();
         if (payload instanceof SyncPayload) {
-            handleSyncPayload((SyncPayload) payload);
+            handleSyncPayload(msg.src(), (SyncPayload) payload);
         } else if (payload instanceof DataToSync) {
             handleDataToSync((DataToSync) payload);
         }
@@ -33,25 +33,32 @@ class MessageReceiver extends ReceiverAdapter {
 
     private void handleDataToSync(DataToSync payload) {
         SyncKV.SyncKVTable table = syncKV.getTable(payload.name);
-        payload.payload.forEach((k,v) -> {
+        payload.payload.forEach((k, v) -> {
             table.put(k, v);
         });
         syncKV.commit();
     }
 
 
-    private void handleSyncPayload(SyncPayload s) {
+    private void handleSyncPayload(Address src, SyncPayload s) {
 
         System.err.println(s.metadata);
         // where missing, we send everything
         Set<String> remoteMissing = syncKV.getTables();
         remoteMissing.removeAll(s.getRemoteTables());
-        System.err.println("missing is " +remoteMissing);
+        System.err.println("missing is " + remoteMissing);
+        for (String toSyncTotally : remoteMissing) {
+            syncTableTotally(src, toSyncTotally);
+        }
 
 
         // where both present, we use the remote bloom filter to send only the one that are _not_ present -> obviously before we check that count and bloom filter are not equals!
         Set<String> bothPresent = syncKV.getTables();
         bothPresent.retainAll(s.getRemoteTables());
-        System.err.println("both present are " +bothPresent);
+        System.err.println("both present are " + bothPresent);
+    }
+
+    private void syncTableTotally(Address src, String name) {
+
     }
 }
