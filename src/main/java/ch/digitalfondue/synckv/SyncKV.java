7b310418cb4df80da351a04bf04b08cae8f5c0da
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-06-09 15:41:32 +0200

    initial work for creating a jchannel

--- src/main/java/ch/digitalfondue/synckv/SyncKV.java
+++ src/main/java/ch/digitalfondue/synckv/SyncKV.java
@@ -2,8 +2,17 @@ package ch.digitalfondue.synckv;
 
 import org.h2.mvstore.MVStore;
 import org.jgroups.JChannel;
+import org.jgroups.conf.ClassConfigurator;
+import org.jgroups.protocols.*;
+import org.jgroups.protocols.pbcast.GMS;
+import org.jgroups.protocols.pbcast.NAKACK2;
+import org.jgroups.protocols.pbcast.STABLE;
+import org.jgroups.stack.Protocol;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SyncKV {
 
@@ -11,16 +20,70 @@ public class SyncKV {
     final MVStore store;
     final long seed;
 
+    static {
+        ensureProtocol();
+    }
+
     public SyncKV(String fileName, String password, JChannel channel) {
+
         this.channel = channel;
         MVStore.Builder builder = new MVStore.Builder().fileName(fileName);
-        if(password != null) {
+        if (password != null) {
             builder.encryptionKey(password.toCharArray());
         }
 
         this.seed = new SecureRandom().nextLong();
 
         this.store = builder.open();
+
+        if (channel != null) {
+            try {
+                channel.connect("SyncKV");
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    public SyncKV(String fileName, String password) {
+        this(fileName, password, buildChannel(password));
+    }
+
+    public static void ensureProtocol() {
+        if (ClassConfigurator.getProtocolId(SymEncryptWithKeyFromMemory.class) == 0) {
+            ClassConfigurator.addProtocol((short) 1024, SymEncryptWithKeyFromMemory.class);
+        }
+    }
+
+
+    // for programmatic configuration, imported from https://github.com/belaban/JGroups/blob/master/src/org/jgroups/demos/ProgrammaticChat.java
+    // switched to TCP_NIO2 and MPING, will need some tweak?
+    static protected JChannel buildChannel(String password) {
+        try {
+            List<Protocol> protocols = new ArrayList<>();
+            protocols.addAll(Arrays.asList(new TCP_NIO2(),
+                    new MPING(),
+                    new MERGE3(),
+                    new FD_SOCK(),
+                    new FD_ALL(),
+                    new VERIFY_SUSPECT(),
+                    new BARRIER()));
+            if(password != null) {
+                protocols.add(new SymEncryptWithKeyFromMemory(password));
+            }
+            protocols.addAll(Arrays.asList(
+                    new NAKACK2(),
+                    new UNICAST3(),
+                    new STABLE(),
+                    new GMS(),
+                    new MFC(),
+                    new FRAG2()
+            ));
+
+            return new JChannel(protocols).name("SyncKV");
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 
 
--- src/test/java/ch/digitalfondue/synckv/SyncKVTest.java
+++ src/test/java/ch/digitalfondue/synckv/SyncKVTest.java
@@ -1,15 +1,8 @@
 package ch.digitalfondue.synckv;
 
-import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
-import org.jgroups.conf.ClassConfigurator;
-import org.jgroups.protocols.*;
-import org.jgroups.protocols.pbcast.GMS;
-import org.jgroups.protocols.pbcast.NAKACK2;
-import org.jgroups.protocols.pbcast.STABLE;
-import org.jgroups.stack.Protocol;
 import org.jgroups.util.Util;
 
 import java.nio.charset.StandardCharsets;
@@ -17,33 +10,11 @@ import java.nio.charset.StandardCharsets;
 public class SyncKVTest {
 
     public static void main(String[] args) throws Exception {
+        SyncKV.ensureProtocol();
 
-        //for programmatic configuration, imported from https://github.com/belaban/JGroups/blob/master/src/org/jgroups/demos/ProgrammaticChat.java
-        // switched to TCP_NIO2 and MPING
-        // TODO: custom encryption provider for loading the encryption key from memory and not from  a keystore
+        SyncKV syncKV = new SyncKV(null, "my_password");
 
-        ClassConfigurator.addProtocol((short) 1024, SymEncryptWithKeyFromMemory.class);
-
-        Protocol[] prot_stack = {
-                new TCP_NIO2(),
-                new MPING(),
-                new MERGE3(),
-                new FD_SOCK(),
-                new FD_ALL(),
-                new VERIFY_SUSPECT(),
-                new BARRIER(),
-                new SymEncryptWithKeyFromMemory("my_password"),
-                new NAKACK2(),
-                new UNICAST3(),
-                new STABLE(),
-                new GMS(),
-                //new UFC(),
-                new MFC(),
-                new FRAG2()};
-        JChannel channel = new JChannel(prot_stack).name("SyncKV");
-
-
-        channel.setReceiver(new ReceiverAdapter() {
+        syncKV.channel.setReceiver(new ReceiverAdapter() {
             public void viewAccepted(View new_view) {
                 System.out.println("view: " + new_view);
             }
@@ -52,11 +23,6 @@ public class SyncKVTest {
                 System.out.println("<< " + msg.getObject() + " [" + msg.getSrc() + "]");
             }
         });
-
-        channel.connect("SyncKV");
-
-
-        SyncKV syncKV = new SyncKV(null, null, channel);
 
         SyncKVTable table = syncKV.getTable("test");
 
@@ -71,7 +37,7 @@ public class SyncKVTest {
 
         for (; ; ) {
             String line = Util.readStringFromStdin(": ");
-            channel.send(null, line);
+            syncKV.channel.send(null, line);
         }
     }
 }
