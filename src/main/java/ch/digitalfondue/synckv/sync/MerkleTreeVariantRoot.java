2327bf65641ad34fa2301bc8767e124fbc28f57d
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-06-10 16:37:41 +0200

    tree: impl and improvements

--- src/main/java/ch/digitalfondue/synckv/sync/MerkleTreeVariantRoot.java
+++ src/main/java/ch/digitalfondue/synckv/sync/MerkleTreeVariantRoot.java
@@ -1,84 +1,109 @@
 package ch.digitalfondue.synckv.sync;
 
 import java.nio.ByteBuffer;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.function.ToIntFunction;
 
 /**
  * Inspired by https://bitcoin.stackexchange.com/questions/51423/how-do-you-create-a-merkle-tree-that-lets-you-insert-and-delete-elements-without/52811#52811 .
  * <p>
  * See https://wiki.apache.org/cassandra/AntiEntropy .
  * <p>
- * Childs may be 2 (for binary selection) or more
+ * Variation:
+ * - number of childs and depth is configurable
+ * - the bucket selection is done using (hash % number of childs)
+ * - the nodes are created lazily
  */
 public class MerkleTreeVariantRoot {
 
-
     private final Node[] childs;
-    private final ToIntFunction<ByteBuffer> hashFunction;
     private int hash;
 
-    public MerkleTreeVariantRoot(int depth, int breadth, ToIntFunction<ByteBuffer> hashFunction) {
-        this.hashFunction = hashFunction;
+    public MerkleTreeVariantRoot(int depth, int breadth) {
         int childDepth = depth - 1;
         this.childs = new Node[breadth];
         for (int i = 0; i < breadth; i++) {
-            this.childs[i] = new Node(childDepth, breadth, hashFunction);
+            this.childs[i] = new Node(childDepth, breadth, null);
         }
     }
 
     public synchronized void add(byte[] value) {
         ByteBuffer wrapped = ByteBuffer.wrap(value);
-        int hash = hashFunction.applyAsInt(wrapped);
-        int bucket = hash % childs.length;
-        childs[bucket].add(wrapped, hash - bucket);
+        int hashWrappedValue = MurmurHash.hash(wrapped);
+        int bucket = hashWrappedValue % childs.length;
+        childs[bucket].add(wrapped, hashWrappedValue - bucket);
+
+        hash = computeHashFor(childs);
+    }
+
+    public int getHash() {
+        return hash;
+    }
+
+    private static int computeHashFor(Node[] childs) {
+        ByteBuffer hashes = ByteBuffer.allocate(childs.length * Integer.BYTES);
+        for (Node c : childs) {
+            hashes.putInt(c != null ? c.hash : 0);
+        }
+        return MurmurHash.hash(hashes);
     }
 
 
     private static class Node {
         private Node[] childs;
-        private Set<ByteBuffer> content;
+        private SortedSet<ByteBuffer> content;
         private int hash;
         private final int depth;
         private final int breadth;
-        private final ToIntFunction<ByteBuffer> hashFunction;
+        private Node parent;
 
-
-        Node(int depth, int breadth, ToIntFunction<ByteBuffer> hashFunction) {
+        Node(int depth, int breadth, Node parent) {
             this.depth = depth;
             this.breadth = breadth;
-            this.hashFunction = hashFunction;
-        }
-
-        private void lazyBuild() {
-            if (depth == 0) {
-                this.childs = null;
-                this.content = new TreeSet<>();
-            } else {
-                this.content = null;
-                this.childs = new Node[breadth];
-            }
+            this.parent = parent;
         }
 
         void add(ByteBuffer wrapped, int resultingHash) {
-            //
-            if (childs == null && content == null) {
-                lazyBuild();
-            }
-            //
 
-            //TODO: calc hash and propagate to parent!
+            //
+            if (depth == 0) {
 
-            if (content != null) {
-                content.add(wrapped);
-            } else {
-                int bucket = resultingHash % childs.length;
-                if(childs[bucket] == null) {
-                    childs[bucket] = new Node(depth - 1, breadth, hashFunction);
+                if (this.content == null) {
+                    this.content = new TreeSet<>();
                 }
+
+                content.add(wrapped);
+
+                //compute hash of content
+                ByteBuffer hashes = ByteBuffer.allocate(content.size() * Integer.BYTES);
+                for (ByteBuffer bf : content) {
+                    hashes.putInt(MurmurHash.hash(bf));
+                }
+                hash = MurmurHash.hash(hashes);
+                if (parent != null) {
+                    parent.updateHash();
+                }
+                //
+            } else {
+
+                if (childs == null) {
+                    this.childs = new Node[breadth];
+                }
+
+                int bucket = resultingHash % childs.length;
+                // lazy node creation too
+                if (childs[bucket] == null) {
+                    childs[bucket] = new Node(depth - 1, breadth, this);
+                }
+                //
                 childs[bucket].add(wrapped, resultingHash - bucket);
+            }
+        }
+
+        private void updateHash() {
+            hash = computeHashFor(childs);
+            if (parent != null) {
+                parent.updateHash();
             }
         }
     }
--- src/test/java/ch/digitalfondue/synckv/sync/MerkleTreeVariantRootTest.java
+++ src/test/java/ch/digitalfondue/synckv/sync/MerkleTreeVariantRootTest.java
@@ -1,5 +1,6 @@
 package ch.digitalfondue.synckv.sync;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MerkleTreeVariantRootTest {
@@ -8,8 +9,17 @@ public class MerkleTreeVariantRootTest {
     @Test
     public void testTreeBuilding() {
         //3 ** 7 = 2187 buckets
-        MerkleTreeVariantRoot r = new MerkleTreeVariantRoot(3, 7, MurmurHash::hash);
+        MerkleTreeVariantRoot r1 = new MerkleTreeVariantRoot(3, 7);
+        r1.add(new byte[]{0,1});
+        r1.add(new byte[]{1,0});
 
-        r.add(new byte[]{0,1});
+
+        //inverted insertion order
+        MerkleTreeVariantRoot r2 = new MerkleTreeVariantRoot(3, 7);
+        r2.add(new byte[]{1,0});
+        r2.add(new byte[]{0,1});
+
+        //resulting hash must be equal, as the tree has the exact same shape
+        Assert.assertEquals(r1.getHash(), r2.getHash());
     }
 }
